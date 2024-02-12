126a927b7b8535a3cdf24920692edde559bb5b4c
dguarisma <davidguarisma@gmail.com>
2024-02-12 16:55:28 -0500

    remove log

--- app/api/route.js
+++ app/api/route.js
@@ -2,8 +2,6 @@ import { Client, APIErrorCode } from '@notionhq/client'
 
 export async function POST(request) {
     try {
-        console.log('request', request);
-
         const DATABASEID = '3773dc283ad44a31a952537fd3834bba';
         const NOTION_TOKEN = 'secret_erhlpWpiRRUOdRPSC67GM2gjW0jgEHwX8LuI5hZpB0T';
         const notion = new Client({ auth: NOTION_TOKEN })
@@ -16,8 +14,6 @@ export async function POST(request) {
                 }
             },
         })
-        console.log('results', results);
-
         const response = results.map(({ properties }) => ({
             categoryID: properties?.IdUrlBanners?.number || null,
             PlatformBanner: properties?.PlatformBanner?.select?.name || null,
