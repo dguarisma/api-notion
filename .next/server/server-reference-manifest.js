eb92878858913f9243dfa1370ffffa67d4cec64e
dguarisma <davidguarisma@gmail.com>
2024-02-12 15:28:35 -0500

    fix error

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"85pPDaW63KwEXipkAmHKGsDqxjEbCHWOtdsAupHqS0c=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"cq2yxpvdyqyYnBntaKzayPsrqjBkT1wat1kdxAunGvc=\"\n}"
